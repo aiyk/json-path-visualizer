b483a5cf54bd62c2ed89def049443c19d40511c4
Ikechukwu Ekwe <iekwe@verifynigeria.com>
2020-09-15 16:55:58 +0100

    UPDATE: read json file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null
+  }
+
+  handleChange = (e) => {
+    const files = e.target.files;   
+    if (files.length <= 0) {
+      return false;
+    }
+
+    const fr = new FileReader();
+    fr.onload = (e) => {
+      const result = JSON.parse(e.target.result);
+      this.setState({
+        data: result
+      });
+    }
+    fr.readAsText(files.item(0));
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} type="file" />
+          <button>Upload</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
