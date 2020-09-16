d829e94e441834f66bc8661b1ba3537dfcdae4e2
Ikechukwu Ekwe <iekwe@verifynigeria.com>
2020-09-16 10:38:56 +0100

    UPDATE: error reporting

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import TreeRoot from './tree-root/tree-root'
 
 class App extends Component {
   state = {
-    data: null
+    data: null,
+    error: false
   }
 
   handleFilterChange = (e) => {
@@ -23,23 +24,44 @@ class App extends Component {
     const fileName = files[0].name;
     let file_ext = fileName.split('.');
     if (file_ext[file_ext.length - 1].toLowerCase() === ext.toLowerCase()) {
-      console.log(true);
       const fr = new FileReader();
       fr.onload = (e) => {
         const result = JSON.parse(e.target.result);
         this.setState({
-          data: result
+          data: result,
+          error: false
         });
       }
       fr.readAsText(files.item(0));
     } else {
-      console.log(false);
+      this.setState({
+        data: null,
+        error: true
+      });
+
+      let timer;
+      let _self = this;
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        _self.setState({
+          data: null,
+          error: false
+        });
+      }, 4000);
+      
     }
   }
 
   render(){
     return (
       <div className="jsonRenderer">
+        {this.state.error?
+          <div className="jsonRenderer__error">
+            <div className="jsonRenderer__error__title">Wrong file format</div>
+            kindly upload a json file instead!
+          </div> : null
+        }
+
         <input type="text" onChange={this.handleFilterChange} className="jsonRenderer__filter" placeholder="type in your query..." />
         <div className="jsonRenderer__contentArea nice nice-scroll">
           <TreeRoot data={this.state.data} />
